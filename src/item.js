7c844823a7a3e13cd3f21750eca9c5b30a1c9abb
Adam <rokakaak@Gmail.com>
2024-06-10 22:44:43 +0300

    + головна сторінка з найкращими фільмами + адаптивний код

--- src/item.js
+++ src/item.js
@@ -1,4 +1,26 @@
 function itemLoad(id) {
     console.log("items", items)
-    console.log("id", id, items.films[id])
+
+    // позначаємо - знаходимо
+    const contentItem = $("#item-src");
+    const contentCardItem = $("item-card-content");
+
+    const contentItemInfo = items.films[id];
+    // картинка
+    const itemImageCard = $("<img>").prop("src", "./assets/" + contentItemInfo.img).addClass("img-fluid rounded-start");
+    // ім'я
+    const itemTitleCard = $("<h5>").addClass("card-title").text(contentItemInfo.name)
+    // категорія
+    const itemCategoryCard = $("<p>").addClass("card-text").text(contentItemInfo.category);
+    // країна
+    const itemCountryCard = $("<p>").addClass("card-text").text(contentItemInfo.country);
+    // дата
+    const itemDataCard = $("<p>").addClass("card-text").text(contentItemInfo.data);
+
+    // додавання
+    contentItem.append(itemImageCard);
+    contentCardItem.append(itemTitleCard, itemCategoryCard, itemCountryCard, itemDataCard);
+
+
+
 }
